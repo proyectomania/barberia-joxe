a0c67a7a766582b093756c152b2f1af3b9a8b347
Alejandro Rodriguez <ceropoint@MacBook-Air-de-Alejandro.local>
2026-02-16 12:26:16 -0500

    fix: remove basename from Router for Vercel root deployment

--- barberia-joxe/src/App.tsx
+++ barberia-joxe/src/App.tsx
@@ -45,7 +45,7 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <AuthProvider>
-      <Router basename={import.meta.env.BASE_URL}>
+      <Router>
         <ScrollToTop />
         <AppRoutes />
       </Router>
